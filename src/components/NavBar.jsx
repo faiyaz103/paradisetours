fefef9085d0e2dcbc7befc5492d1020154149ebb
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2024-12-22 17:46:24 +0600

    Router Setup Completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,75 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+    const [isMenuOpen, setMenuOpen]=useState(false);
+        const toggleMenu = () =>{
+            setMenuOpen(!isMenuOpen);
+        };
+
   return (
-    <div>NavBar</div>
+    <nav className="bg-gray-800">
+
+    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="relative flex h-16 items-center justify-between">
+
+        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        {/* <!-- Mobile menu button--> */}
+        <button onClick={toggleMenu} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded={isMenuOpen}>
+            <span className="absolute -inset-0.5"></span>
+            <span className="sr-only">Open main menu</span>
+            {/* <!--
+            Icon when menu is closed.
+
+            Menu open: "hidden", Menu closed: "block"
+            --> */}
+            <svg className="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            {/* <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+            --> */}
+            <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </button>
+        </div>
+
+        <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4">
+            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+            <Link to="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</Link>
+            <Link to="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</Link>
+            <Link to="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</Link>
+            <Link to="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</Link>
+            </div>
+        </div>
+        </div>
+
+        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <Link to=""><h1 className='text-white font-bold'>Paradise Tours</h1></Link>
+        </div>
+
+    </div>
+    </div>
+
+    {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+    <div className={`${isMenuOpen ? "block" : "hidden"} sm:hidden`} id="mobile-menu">
+    <div className="space-y-1 px-2 pb-3 pt-2">
+        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+        <Link to="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</Link>
+        <Link to="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</Link>
+        <Link to="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</Link>
+        <Link to="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</Link>
+    </div>
+    </div>
+
+    </nav>
   )
 }
 
